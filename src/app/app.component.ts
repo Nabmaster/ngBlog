505893ca55059f8a422fc274e080b4190ae70bdc
nabmaster <neonab@hotmail.com>
2018-02-28 16:48:03 +0100

    ajout de fonction et fichier articles.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgForm } from "@angular/forms";
+import { Article } from './article';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string;
+  articles: Array<Article>;
+  edit: boolean;
+  editArticle: Article;
+
+  constructor() {
+    this.title = 'Better ngBlog';
+    this.articles = new Array();
+    this.editArticle = new Article(0,'');
+    this.edit = false;
+    this.articles.push(new Article(99, 'Article de test', 'description test'));
+  }
+  addArticle() {
+    this.edit = true;
+  }
+  backToList() {
+    this.edit = false;
+  }
+  saveArticle(myForm: NgForm) {
+    //Json serialiser et deserialiser article pour avoir une nouvelle instance objet
+    this.articles.push(JSON.parse(JSON.stringify(this.editArticle)));
+    myForm.resetForm();
+    this.edit = false;
+  }
 }
