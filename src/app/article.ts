111659a43caefc8c4d394a344fb3e328d7cda9a2
nabmaster <neonab@hotmail.com>
2018-02-28 12:23:01 +0100

    first commit

---
+++ src/app/article.ts
@@ -0,0 +1,17 @@
+import * as moment from 'moment';
+export class Article {
+  id: number;
+  title: string;
+  description: string;
+  author: string;
+  date: moment.Moment;
+
+  constructor(id: number, title: string, description?:string, author: string = 'nab'){
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.author = author;
+    this.date = moment();
+  }
+
+}
